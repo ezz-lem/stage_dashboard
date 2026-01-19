f0387101e598bd50fa6ec9f87fe8ab7da3dc376c
ezz-lem <ezz.lem0812@gmail.com>
2026-01-19 22:53:56 +0100

    Updates

--- src/components/Loader.jsx
+++
@@ -1,9 +0,0 @@
-import { Loader2 } from 'lucide-react';
-
-const Loader = ({ size = 24, className = "" }) => {
-    return (
-        <Loader2 className={`animate-spin text-blue-600 ${className}`} size={size} />
-    );
-};
-
-export default Loader;
