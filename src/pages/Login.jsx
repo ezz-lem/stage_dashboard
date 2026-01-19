72dba51a2f154b66d4c53f6f9f74547290b551cd
ezz-lem <ezz.lem0812@gmail.com>
2026-01-19 22:50:03 +0100

    feat: re-apply login autocomplete fixes for inputs

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -82,6 +82,7 @@ const Login = () => {
                                     required
                                     value={loginEmail}
                                     onChange={(e) => setLoginEmail(e.target.value)}
+                                    autoComplete="username"
                                     className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
                                 />
                             </div>
@@ -99,6 +100,7 @@ const Login = () => {
                                     required
                                     value={password}
                                     onChange={(e) => setPassword(e.target.value)}
+                                    autoComplete="current-password"
                                     className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
                                 />
                             </div>
